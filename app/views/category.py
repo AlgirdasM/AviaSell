c168a45d5dabb47d5f9bc29426718e497301f39a
AlgirdasM <algirdaslt@gmail.com>
2018-08-29 12:13:11 +0300

    docs: add comments to category.py

--- app/views/category.py
+++ app/views/category.py
@@ -4,11 +4,14 @@ from flask import render_template
 from app import webapp
 from app.models import *
 
+
 # Read category page
 @webapp.route('/category/<string:category_name>/')
 def getCategory(category_name):
+	# Check if category exist, if not then 404
     q = CategoryModel.isThereCategory(category_name)
     if q:
+    	# Get items from database
         return render_template('category.html')
     else:
         return render_template('404.html'), 404
