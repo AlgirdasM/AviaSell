f40188f7b4fa29a79481c682ee3d1445f819c267
AlgirdasM <algirdaslt@gmail.com>
2018-09-02 15:08:19 +0300

    feat: add Google login

--- app/models/user.py
+++ app/models/user.py
@@ -23,3 +23,20 @@ class UserModel():
     def getUserEmail(uid):
         result = session.query(User).filter_by(id=uid).one()
         return result.email
+
+
+    def getUserID(email):
+        try:
+            user = session.query(User).filter_by(email=email).one()
+            return user.id
+        except:
+            return None
+
+
+    def createUser(login_session):
+        newUser = User(name=login_session['username'], email=login_session[
+                       'email'], picture=login_session['picture'])
+        session.add(newUser)
+        session.commit()
+        user = session.query(User).filter_by(email=login_session['email']).one()
+        return user.id
